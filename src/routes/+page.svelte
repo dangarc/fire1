2de5602959873ad2b1f9d849fe4fa4d27f373915
bruce mckenzie <bjmckenz@gmail.com>
2024-11-14 17:05:52 -0800

    anonymous working!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { signInAnonymously } from "firebase/auth";
+    import { SignedIn, SignedOut } from "sveltefire";
+</script>
+
+DON'T CARE BEFORE<br/>
+
+<SignedIn let:user let:signOut>
+    SIGNED IN
+    <p>Hello {user.uid}</p>
+    <button on:click={signOut}>Sign Out</button>
+</SignedIn>
+
+<SignedOut let:auth>
+    SIGNED OUT
+    <button on:click={() => signInAnonymously(auth)}>Sign In</button>
+</SignedOut>
+
+<br/>DON'T CARE AFTER
---
+++ src/routes/utest/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+     import {  userStore } from 'sveltefire';
+
+    export let auth;
+    export let firestore;
+    const user = userStore();
+
+  </script>
+  PS {$user?.displayName} END PS
