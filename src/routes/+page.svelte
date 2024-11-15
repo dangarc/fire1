31a2c524e8f4bef6e9fbe967009dea85518543a2
bruce mckenzie <bjmckenz@gmail.com>
2024-11-14 20:35:53 -0800

    phone working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,148 @@
 <script>
-    import { signInAnonymously } from "firebase/auth";
+    import { signInAnonymously, signInWithPopup } from "firebase/auth";
+    import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
     import { SignedIn, SignedOut } from "sveltefire";
+    import { GoogleAuthProvider } from "firebase/auth";
+    import { GithubAuthProvider } from "firebase/auth";
+    import { browser } from "$app/environment";
+
+    const GHprovider = new GithubAuthProvider();
+    const Gprovider = new GoogleAuthProvider();
+
+    let signedinuser = {};
+    let token = "";
+    let solved = false;
+
+    const do_popup = async (auth, provider, type) => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = type.credentialFromResult(result);
+                token = credential.accessToken;
+                // The signed-in user info.
+                signedinuser = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = type.credentialFromError(error);
+                console.error("Auth error", {errorCode, errorMessage, email, credential});
+            });
+    };
+
+    let phoneNumber = "+1 714-555-9898";
+    let verificationCode = "";
+    let confirmationResult = null;
+    let recaptchaVerifier = null;
+
+    const beginCodeSendingProcess = async (auth) => {
+        if (browser && !recaptchaVerifier && !solved) {
+            recaptchaVerifier = new RecaptchaVerifier(
+                "recaptcha-container",
+                {
+                    size: "normal",
+                    callback: (response) => {
+                        console.log("recaptcha solved", response);
+                        solved = true;
+                        doSignIn(auth);
+                    },
+                },
+                auth
+            );
+            console.log("created recaptchaVerifier");
+            window.recaptchaVerifier = recaptchaVerifier;
+            recaptchaVerifier.render();
+        } else {
+            console.log("not browser");
+        }
+    };
+    const doSignIn = async (auth) => {
+        try {
+            console.log("doSignIn", { auth, phoneNumber, window });
+            confirmationResult = await signInWithPhoneNumber(
+                auth,
+                phoneNumber,
+                recaptchaVerifier
+            );
+            console.log("back from signInWithPhoneNumber");
+            const credential = confirmationResult(result);
+            token = credential.accessToken;
+            signedinuser = result.user;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const verifyCode = async () => {
+        try {
+            const credential =
+                await confirmationResult.confirm(verificationCode);
+            // User is now signed in, credential.user contains the user object
+            console.log(credential.user);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 </script>
 
-DON'T CARE BEFORE<br/>
-
+<p>DON'T CARE BEFORE</p>
+<hr />
 <SignedIn let:user let:signOut>
-    SIGNED IN
+    <p>SIGNED IN</p>
     <p>Hello {user.uid}</p>
     <button on:click={signOut}>Sign Out</button>
 </SignedIn>
+<hr />
 
 <SignedOut let:auth>
-    SIGNED OUT
-    <button on:click={() => signInAnonymously(auth)}>Sign In</button>
+    <p>SIGNED OUT -- ANONYMOUS SIGNIN</p>
+    <button on:click={() => signInAnonymously(auth)}>Sign In anonymously</button
+    >
+</SignedOut>
+<hr />
+
+<SignedOut let:auth>
+    <p>GOOGLE POPUP</p>
+    <button on:click={() => do_popup(auth, Gprovider, GoogleAuthProvider)}
+        >Sign In with popup</button
+    >
+</SignedOut>
+<hr />
+
+<SignedOut let:auth>
+    <p>GITHUB POPUP</p>
+    <button on:click={() => do_popup(auth, GHprovider, GithubAuthProvider)}
+        >Sign In with popup</button
+    >
 </SignedOut>
 
-<br/>DON'T CARE AFTER
+<hr />
+<SignedOut let:auth>
+    <p>PHONE VERIFICATION</p>
+
+    <p>Captcha {solved ? "solved" : "not solved"}</p>
+    {#if confirmationResult}
+        <input
+            type="text"
+            bind:value={verificationCode}
+            placeholder="Verification code"
+        />
+        <button on:click={verifyCode}>Verify code</button>
+    {:else}
+        <div id="recaptcha-container"></div>
+        <input type="tel" bind:value={phoneNumber} placeholder="Phone number" />
+        <button on:click={() => beginCodeSendingProcess(auth)}
+            >Send Verification code</button
+        >
+    {/if}
+</SignedOut>
+
+<p>DON'T CARE AFTER</p>
+<hr />
 <a href="/utest">utest</a>
--- src/routes/utest/+page.svelte
+++ src/routes/utest/+page.svelte
@@ -1,9 +1,22 @@
 <script>
-     import {  userStore } from 'sveltefire';
+    import { SignedIn, SignedOut } from "sveltefire";
+</script>
 
-    export let auth;
-    export let firestore;
-    const user = userStore();
+<SignedIn let:user let:signOut>
+  <p>User {user.uid} is signed in</p>
+</SignedIn>
 
-  </script>
-  PS {$user?.displayName} END PS
+<SignedOut let:auth>
+  <p>We KNOW no user is signed in here</p>
+</SignedOut>
+
+<p><i>We don't know if anyone is signed in or out for this section.</i></p>
+
+<SignedIn let:signOut>
+  <p>Here, we know someone is signed in. All we do is let them sign out.</p>
+
+  <button on:click={signOut}>Sign Out</button>
+</SignedIn>
+
+
+<p><a href="/">Home</a></p>
